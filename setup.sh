d159d366312854f35657826888ee5c4fae95aee2
Jason Brown <jasedbrown+github@gmail.com>
2022-07-16 06:52:25 -0700

    pip

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install \
+sudo apt install -y \
      zsh \
      emacs \
      gcc \
@@ -11,11 +11,18 @@ sudo apt install \
      net-tools \
      autoconf \
      libtool \
-     curl
+     curl \
+     python3-pip
+
 
 cp -v -R home/.* home/* ~
 
+# install oh-my-zsh *yeah, i still like it)
 if [ ! -d "~/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# install rust/rustup
+if [ ! -d "~/.rustup" ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
