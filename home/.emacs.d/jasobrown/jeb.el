a60329b33731e71914df978d90ee2584d02a1071
Jason Brown <jason.b@readyset.io>
2023-12-18 05:35:12 -0800

    bump jvm args for jdtls

--- home/.emacs.d/jasobrown/jeb.el
+++ home/.emacs.d/jasobrown/jeb.el
@@ -429,6 +429,9 @@
   :ensure t
   :config (add-hook 'java-mode-hook 'lsp))
 
+;; bump the jdtls JVM args. taken from https://github.com/emacs-lsp/lsp-java, which is taken from VSCode
+(setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx2G" "-Xms100m"))
+
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ;; setting up debugging support with dap-mode
 
