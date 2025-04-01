5b97f85a5260a69356b60aa6e76bc7cd2a35dbb7
Jason Brown <jason.b@readyset.io>
2025-04-01 11:34:13 -0700

    zsh: properly set shell history
    another oopsies ... setting this explicitly now

--- home/.zsh_home/.zsh/history.zsh
+++ home/.zsh_home/.zsh/history.zsh
@@ -1,2 +1,15 @@
+# Ensure history is enabled
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+
+# Append history instead of overwriting
+setopt APPEND_HISTORY
 # jfc, i hate shared history
 unsetopt share_history
+
+# Write history immediately, rather than waiting
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+
