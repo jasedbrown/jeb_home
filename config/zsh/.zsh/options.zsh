05f49aa0a670a7e7fc8ac2f196b741549e9365c7
Jason Brown <jason.b@readyset.io>
2025-05-06 15:47:25 -0700

    zsh: set option interactivecomments
    so i can start a line with `#` not not have it execute

--- config/zsh/.zsh/options.zsh
+++ config/zsh/.zsh/options.zsh
@@ -1,1 +1,5 @@
 # this file is for shell config/options
+
+# any text that follows a # character on a command line will be treated
+# as a comment and ignored by the shell during execution
+setopt interactivecomments
