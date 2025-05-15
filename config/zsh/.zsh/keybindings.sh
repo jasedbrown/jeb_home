fca5dd6e2290788f204258ee0ca7e8a20369b498
Jason Brown <jason.b@readyset.io>
2025-05-15 13:52:01 -0700

    zsh: start adding keybindings stuff

--- config/zsh/.zsh/keybindings.sh
+++ config/zsh/.zsh/keybindings.sh
@@ -1,2 +1,8 @@
+# https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html
+
 # force the emacs key ZLE bindings
 bindkey -e
+
+# a more emacs-like keybinding for backward-kill-word
+WORDCHARS=''
+bindkey '^[^?' backward-kill-word
