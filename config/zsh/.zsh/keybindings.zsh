aaf33f958014a44886aace93173da66f1b81bef4
Jason Brown <jasedbrown@gmail.com>
2025-05-17 09:26:28 -0700

    zsh: fix keybindings
    - renamed the file from `.sh` to `.zsh` so it actually gets read at
    session start - doh!
    - set the `Delete` properly for arch

--- config/zsh/.zsh/keybindings.zsh
+++ config/zsh/.zsh/keybindings.zsh
@@ -6,3 +6,6 @@ bindkey -e
 # a more emacs-like keybinding for backward-kill-word
 WORDCHARS=''
 bindkey '^[^?' backward-kill-word
+
+# force the Delete key to delete the next char, and not print a `~`.
+bindkey '^[[3~' delete-char
