ee756aa24417671ee554104b53cce2cc692ecc84
Jason Brown <jasedbrown@gmail.com>
2025-10-26 07:20:56 -0700

    zsh: update npm settings

--- config/zsh/.zsh/environment.zsh
+++ config/zsh/.zsh/environment.zsh
@@ -51,4 +51,4 @@ eval "$(pyenv virtualenv-init - zsh)"
 # nvm/nodejs - really only using for claude code (atm)
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export PATH="$PATH:${XDG_DATA_HOME:-$HOME/.local/share}/npm/bin"
