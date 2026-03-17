29b0dee9a0e36620e5d73ff4a20474c9426324bb
Jason Brown <jason.b@readyset.io>
2026-03-17 10:10:20 -0700

    zsh: prompt now looks for `jj` bookmark

--- config/zsh/.zsh/prompt.zsh
+++ config/zsh/.zsh/prompt.zsh
@@ -1,5 +1,17 @@
 autoload -Uz vcs_info
-precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats '%b '
+
+_vcs_branch_info() {
+    local jj_bookmark
+    jj_bookmark=$(jj log --no-graph -r "@ & bookmarks()" -T 'bookmarks.join(", ")' 2>/dev/null)
+    if [[ -n "$jj_bookmark" ]]; then
+        _branch_info="${jj_bookmark} "
+    else
+        vcs_info
+        _branch_info="${vcs_info_msg_0_}"
+    fi
+}
+
+precmd() { _vcs_branch_info }
 setopt prompt_subst
-PROMPT=' %F{blue}%c%f %F{red}${vcs_info_msg_0_}%f%F{yellow}→%f '
+PROMPT=' %F{blue}%c%f %F{red}${_branch_info}%f%F{yellow}→%f '
